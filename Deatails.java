223f06b7dcc0d032c2036b5746090e5a31e349fa
Fathima Nusha <nusharauf@gmail.com>
2017-12-19 09:41:57 +0530

    changed the type of phn no

---
+++ Deatails.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Deatails
+{
+	public static void main(String arg[])
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println(" Enter Your Name : ");
+		String Name	 = input.next();
+		System.out.println(" Enter Your Age : ");
+		int  Age   	 = input.nextInt();
+		System.out.println(" Enter Your Phn : ");
+		int  Phn   	 = input.nextInt();
+		System.out.println(" Enter Your Address : ");
+		String Address = input.next();
+		System.out.println(" Enter Your Course of Study : ");
+		String Course  = input.next();
+		System.out.println(" Name :"+ Name + "\n Age : "+Age + "\n Address : "+Address + "\n Course of Study  : "+Course);
+	}
+}
